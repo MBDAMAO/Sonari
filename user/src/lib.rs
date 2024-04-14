982ea7f3ff4a37de5a767fe45aa5e36ff494377d
MBDAMAO <3034906016@qq.com>
2024-04-14 16:19:08 +0800

    Code refactoring

---
+++ user/src/lib.rs
@@ -0,0 +1,17 @@
+#![feature(linkage)]
+
+use core::panic;
+
+#[no_mangle]
+#[link_section = ".text.entry"]
+pub extern "C" fn _start() -> ! {
+    clear_bss();
+    exit(main());
+    panic!("unreachable after sys_exit!");
+}
+
+#[linkage = "weak"]
+#[no_mangle]
+fn main() -> i32 {
+    panic!("Cannot find main!");
+}
